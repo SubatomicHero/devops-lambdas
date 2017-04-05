27e429ee9356acae655e48b03026794026e66cfc
Daniel Monteiro <daniel.monteiro@alfresco.com>
2017-04-05 16:49:42 +0100

    code updated

--- python/TrialRequestHandler/TRH.py
+++ python/TrialRequestHandler/TRH.py
@@ -22,7 +22,7 @@ def lambda_handler(event, context):
         if get_source == 'onlinetrials':
             response = None
             count_attempts = 0
-            while response == None:
+            while not response:
                 try:
                     response = details_marketo(lead_id)
                     count_attempts = count_attempts + 1
@@ -30,24 +30,31 @@ def lambda_handler(event, context):
                         break
                 except IOError:
                     print("no response from marketo")
-            if response != None:
+            if response:
                 try:
                     fulfilled_test = 'y'
-                    insert_into_dynamo(lead_id, response, fulfilled_test, count_attempts)
-                    sqs_result = send_to_SQS(response)
-                    return "Inserted fulfilled in dynamodb"
+                    ins_db = insert_into_dynamo(lead_id, response, fulfilled_test, count_attempts)
+                    if(ins_db):
+                        ins_sqs = sqs_result = send_to_SQS(response)
+                        if(ins_sqs):
+                            return "Inserted fulfilled in dynamodb and SQS"
+                        return "failed to insert in SQS"
+                    return "failed to insert into dynamodb"
                 except IOError:
                     print("no response from dynamodb")
             else:
                 try:
                     fulfilled_test = 'n'
-                    insert_into_dynamo(lead_id, response, fulfilled_test, count_attempts)
-                    # notify IM solution
-                    return "Inserted unfulfilled in dynamodb"
+                    ins_db = insert_into_dynamo(lead_id, response, fulfilled_test, count_attempts)
+                    if(ins_db):
+                        # notify IM solution
+                        return "Inserted unfulfilled in dynamodb"
+                    return "failed to insert into dynamodb"
+
                 except IOError:
                     print("no response from dynamodb")
     except EOFError:
-        print("lambda handler failed for some error")
+        print("lambda handler failed")
 
 
 # function to obtain the details regarding the lead_id passed as a parameter
@@ -58,10 +65,9 @@ def details_marketo(lead_id):
         client_secret = os.environ['client_secret']
         m = Marketo(host = api_host, client_id = client_id, client_secret = client_secret)
         response_m = m.get_leads('id', lead_id)
-        if(response_m != None):
+        if(response_m):
             return response_m
-        else:
-            return None
+        return None
     except IOError:
         print("an error has been found with the details")
 
@@ -70,7 +76,7 @@ def details_marketo(lead_id):
 def insert_into_dynamo(lead_id, response_m, fulfilled_test, count_attempts):
     try:
         curr_date = time.strftime("%d/%m/%Y")
-        if response_m == None:
+        if not response_m:
             response = dynamo_client.update_item(
                 TableName=os.environ['trial_request_table'],
                 Key={
@@ -90,12 +96,11 @@ def insert_into_dynamo(lead_id, response_m, fulfilled_test, count_attempts):
                     ':rt': {"N": '0'},
                     ':rm': {"S": str(response_m)}
             })
-            if(response != None):
+            if(response):
                 print("UpdateItem succeeded:")
                 return response
-            else:
-                print("UpdateItem Failed:")
-                return None
+            print("UpdateItem Failed:")
+            return None
         else:
             dt = datetime.strptime(response_m['result'][0]['createdAt'], "%Y-%m-%dT%H:%M:%SZ")
             request_time = time.mktime(dt.timetuple()) + (dt.microsecond / 1000000.0)
@@ -119,12 +124,11 @@ def insert_into_dynamo(lead_id, response_m, fulfilled_test, count_attempts):
                     ':rt': {"N": str(request_time)},
                     ':rm': {"S": str(response_m)}
             })
-            if(response != None):
+            if(response):
                 print("UpdateItem succeeded:")
                 return response
-            else:
-                print("UpdateItem Failed:")
-                return None
+            print("UpdateItem Failed:")
+            return None
     except IOError:
         print("an error has been found. Data not inserted into the table")
 
@@ -133,9 +137,8 @@ def insert_into_dynamo(lead_id, response_m, fulfilled_test, count_attempts):
 def send_to_SQS(response):
     try:
         resp = sqs_client.send_message(QueueUrl=os.environ['sqs_url'],MessageBody=json.dumps(response))
-        if(resp != None):
+        if(resp):
             return resp
-        else:
-            return None
+        return None
     except IOError:
         print("an error has been found. Message not been sent to the queue")
