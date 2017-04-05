c932311d8d8774aeb4ad2da1c01729a07d9abb05
Daniel Monteiro <daniel.monteiro@alfresco.com>
2017-04-05 14:12:19 +0100

    final code for review

--- python/TrialRequestHandler/TRH.py
+++ python/TrialRequestHandler/TRH.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 from marketo import Marketo
 
 import json
@@ -7,92 +6,122 @@ import boto3
 import datetime
 import time
 
-
-os.environ['api_host'] = "https://453-LIZ-762.mktorest.com"
-os.environ['client_id'] = "35a7e1a3-5e60-40b2-bd54-674680af2adc"
-os.environ['client_secret'] = "iPPgKiB224jsa02duwPcKy9ox7078P7S"
-os.environ['trial_request_table'] = "OnlineTrialRequestDynamoDbTable"
-
-
-#TO BE USED ONLY ON THE FINAL VERSION
-#DYNAMO_DB = boto3.resource('dynamodb', region_name='us-west-2')
-#dyn_table = DYNAMO_DB.Table(os.environ['trial_request_table'])
-#sqs = boto3.resource('sqs')
-#queue = sqs.get_queue_by_name(QueueName=os.environ['sqs_url'])
-
+dynamo_client = boto3.client('dynamodb')
+sqs_client = boto3.client('sqs')
+sns_client = boto3.client('sns')
 
 print('Getting message from SNS')
 
-#main lambda function
+# main lambda function
 def lambda_handler(event, context):
+    try:
+        message = event['Records'][0]['Sns']['Message']
+        get_source = message['source']
+        lead_id = message['lead']
+        print("From SNS: " + get_source)
+        if get_source == 'onlinetrials':
+            response = ""
+            count_attemps = 0
+            while response == "":
+                try:
+                    response = details_marketo(lead_id)
+                    count_attemps = count_attemps + 1
+                    if count_attemps == 15:
+                        break
+                except IOError:
+                    print("no response from marketo")
+            if response != "":
+                try:
+                    fulfilled_test = 'y'
+                    insert_into_dynamo(lead_id, response, fulfilled_test, count_attemps)
+                    sqs_result = send_to_SQS(response)
+                    return "Inserted fulfilled in dynamodb"
+                except IOError:
+                    print("no response from dynamodb")
+            else:
+                try:
+                    fulfilled_test = 'n'
+                    insert_into_dynamo(lead_id, response, fulfilled_test, count_attemps)
+                    # notify IM solution
+                    return "Inserted unfulfilled in dynamodb"
+                except IOError:
+                    print("no response from dynamodb")
+    except EOFError:
+        print("lambda handler failed for some error")
 
-    message = event['Records'][0]['Sns']['Message']
-    get_source = message['source']
-    lead_id = message['lead']
-    print("From SNS: " + get_source)
-    if (get_source == 'onlinetrials'):
-        response = ""
-        count_attemps = 0
-        while response == "":
-            #TO BE USED ONLY ON THE FINAL VERSION
-            #response = details_marketo(lead_id)
-            count_attemps = count_attemps + 1
-            if count_attemps == 15:
-                break
-        if(response != ""):
-            fulfilled_test = 'y'
-            send_to_SQS(response)
-            #TO BE USED ONLY ON THE FINAL VERSION
-            #insert_into_dynamo(dyn_table,lead_id,response,fulfilled_test,count_attemps)
-            return "OP1"
-        else:
-            fulfilled_test = 'n'
-            #TO BE USED ONLY ON THE FINAL VERSION
-            #insert_into_dynamo(dyn_table,lead_id,response,fulfilled_test,count_attemps)
-            return "OP2"
 
-#function to obtain the details regarding the lead_id passed as a parameter
+# function to obtain the details regarding the lead_id passed as a parameter
 def details_marketo(lead_id):
+    try:
         api_host = os.environ['api_host']
         client_id = os.environ['client_id']
         client_secret = os.environ['client_secret']
         m = Marketo(host = api_host, client_id = client_id, client_secret = client_secret)
-        response_m = m.get_leads('email', lead_id)
+        response_m = m.get_leads('id', lead_id)
         return response_m
+    except IOError:
+        print("an error has been found with the details")
 
-#function to insert the request in the dynamodB table for the Trial Request Handler
-def insert_into_dynamo(table, lead_id, response_m, fulfilled_test, count_attemps):
 
-    curr_date = time.strftime("%d/%m/%Y")
-    if response_m == "":
-        response = table.put_item(
-        Item={
-            'LeadId': lead_id,
-            'Date': curr_date,
-            'Fulfilled': fulfilled_test,
-            'Attempts' : count_attemps,
-            'RequestTime' : response_m,
-            'Request' : response_m
+# function to insert the request in the dynamodB table for the Trial Request Handler
+def insert_into_dynamo(lead_id, response_m, fulfilled_test, count_attemps):
+    try:
+        curr_date = time.strftime("%d/%m/%Y")
+        if response_m == "":
+            response = dynamo_client.update_item(
+                TableName=os.environ['trial_request_table'],
+                Key={
+                    'LeadId': {"N": str(lead_id)},
+                    'Date': {"S": curr_date}
+                },
+                UpdateExpression= "set #Fulfilled = :f, #Attemps = :a, #RequestTime = :rt, #RequestMsg = :rm",
+                ExpressionAttributeNames={
+                    '#Fulfilled': "Fulfilled",
+                    '#Attemps': "Attemps",
+                    '#RequestTime': "RequestTime",
+                    '#RequestMsg': "RequestMsg"
+                },
+                ExpressionAttributeValues={
+                    ':f': {"S": fulfilled_test},
+                    ':a': {"N": str(count_attemps)},
+                    ':rt': {"N": '0'},
+                    ':rm': {"S": str(response_m)}
             })
-        print("PutItem succeeded:")
-        return response
-    else:
-        dt = datetime.datetime.strptime(response_m['result'][0]['createdAt'], "%Y-%m-%dT%H:%M:%SZ")
-        request_time = time.mktime(dt.timetuple()) + (dt.microsecond / 1000000.0)
-        request_time = int(request_time)
-        response = table.put_item(
-        Item={
-            'LeadId': lead_id,
-            'Date': curr_date,
-            'Fulfilled': fulfilled_test,
-            'Attempts' : count_attemps,
-            'RequestTime' : request_time,
-            'Request' : response_m
+            print("UpdateItem succeeded:")
+            return response
+        else:
+            dt = datetime.datetime.strptime(response_m['result'][0]['createdAt'], "%Y-%m-%dT%H:%M:%SZ")
+            request_time = time.mktime(dt.timetuple()) + (dt.microsecond / 1000000.0)
+            request_time = int(request_time)
+            response = dynamo_client.update_item(
+                TableName=os.environ['trial_request_table'],
+                Key={
+                    'LeadId': {"N": str(lead_id)},
+                    'Date': {"S": curr_date}
+                },
+                UpdateExpression= "set #Fulfilled = :f, #Attemps = :a, #RequestTime = :rt, #RequestMsg = :rm",
+                ExpressionAttributeNames={
+                    '#Fulfilled': "Fulfilled",
+                    '#Attemps': "Attemps",
+                    '#RequestTime': "RequestTime",
+                    '#RequestMsg': "RequestMsg"
+                },
+                ExpressionAttributeValues={
+                    ':f': {"S": fulfilled_test},
+                    ':a': {"N": str(count_attemps)},
+                    ':rt': {"N": str(request_time)},
+                    ':rm': {"S": str(response_m)}
             })
-        print("PutItem succeeded:")
-        return response
+            print("UpdateItem succeeded:")
+            return response
+    except IOError:
+        print("an error has been found. Data not inserted into the table")
 
-#function to send the response from marketo to the SQS
-def send_to_SQS(queue,response):
-    resp = queue.send_message(MessageBody=json.dumps(response))
-    return resp
+
+# function to send the response from marketo to the SQS
+def send_to_SQS(response):
+    try:
+        resp = sqs_client.send_message(QueueUrl=os.environ['sqs_url'],MessageBody=json.dumps(response))
+        return resp
+    except IOError:
+        print("an error has been found. Message not been sent to the queue")
--- python/TrialRequestHandler/mock_file.py
+++ python/TrialRequestHandler/mock_file.py
@@ -1,93 +1,103 @@
 import boto3
-from moto import mock_sns
 from moto import mock_sqs
 from moto import mock_dynamodb2
-from TRH import insert_into_dynamo
-from TRH import details_marketo
-from TRH import lambda_handler
+import datetime
+import time
 import json
 
 
 def test_lambda_handler():
-
     event = {
-  "Records": [
-    {
-      "EventVersion": "1.0",
-      "EventSubscriptionArn": "arn:aws:sns:EXAMPLE",
-      "EventSource": "aws:sns",
-      "Sns": {
-        "SignatureVersion": "1",
-        "Timestamp": "1970-01-01T00:00:00.000Z",
-        "Signature": "EXAMPLE",
-        "SigningCertUrl": "EXAMPLE",
-        "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
-        "Message": { "source": "onlinetrials", "lead": "nnnnnnn" },
-        "MessageAttributes": {
-          "Test": {
-            "Type": "String",
-            "Value": "TestString"
-          },
-          "TestBinary": {
-            "Type": "Binary",
-            "Value": "TestBinary"
+      "Records": [
+        {
+          "EventVersion": "1.0",
+          "EventSubscriptionArn": "arn:aws:sns:EXAMPLE",
+          "EventSource": "aws:sns",
+          "Sns": {
+            "SignatureVersion": "1",
+            "Timestamp": "1970-01-01T00:00:00.000Z",
+            "Signature": "EXAMPLE",
+            "SigningCertUrl": "EXAMPLE",
+            "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
+            "Message": { "source": "onlinetrials", "lead": "nnnnnnn" },
+            "MessageAttributes": {
+              "Test": {
+                "Type": "String",
+                "Value": "TestString"
+              },
+              "TestBinary": {
+                "Type": "Binary",
+                "Value": "TestBinary"
+              }
+            },
+            "Type": "Notification",
+            "UnsubscribeUrl": "EXAMPLE",
+            "TopicArn": "arn:aws:sns:EXAMPLE",
+            "Subject": "TestInvoke"
           }
-        },
-        "Type": "Notification",
-        "UnsubscribeUrl": "EXAMPLE",
-        "TopicArn": "arn:aws:sns:EXAMPLE",
-        "Subject": "TestInvoke"
-      }
+        }
+      ]
     }
-  ]
-}
-
-    message = event['Records'][0]['Sns']['Message']
-    get_source = message['source']
-    lead_id = message['lead']
-    print("From SNS: " + get_source)
-    if (get_source == 'onlinetrials'):
-        response = {'result': [{'updatedAt': '2015-08-10T06:53:11Z', 'lastName': 'Taylor', 'firstName': 'Dan', 'createdAt': '2014-09-18T20:56:57Z', 'email': 'daniel.taylor@alfresco.com', 'id': 1558511}], 'success': True, 'requestId': 'e809#14f22884e5f'}
-        count_attemps = 0
-        while response == "":
-            count_attemps = count_attemps + 1
-            if count_attemps == 15:
-                break
-        if(response != ""):
-            fulfilled_test = 'y'
-            test_send_to_SQS()
-            test_insert_into_dynamo()
-            result = "OP1"
-        else:
-            fulfilled_test = 'n'
-            test_insert_into_dynamo()
-            result = "OP2"
-
-        assert result == "OP1"
-        print result
+    try:
+        message = event['Records'][0]['Sns']['Message']
+        get_source = message['source']
+        lead_id = message['lead']
+        print("From SNS: " + get_source)
+        if (get_source == 'onlinetrials'):
+            response = {'result': [{'updatedAt': '2015-08-10T06:53:11Z', 'lastName': 'Taylor', 'firstName': 'Dan', 'createdAt': '2014-09-18T20:56:57Z', 'email': 'daniel.taylor@alfresco.com', 'id': 1558511}], 'success': True, 'requestId': 'e809#14f22884e5f'}
+            count_attemps = 0
+            while response == "":
+                try:
+                    count_attemps = count_attemps + 1
+                    if count_attemps == 15:
+                        break
+                except IOError:
+                    print("no response from marketo")
+            if(response != ""):
+                try:
+                    fulfilled_test = 'y'
+                    test_insert_into_dynamo()
+                    sqs_result = test_send_to_SQS()
+                    result = "Inserted fulfilled in dynamodb"
+                except IOError:
+                    print("no response from dynamodb")
+            else:
+                try:
+                    fulfilled_test = 'n'
+                    test_insert_into_dynamo()
+                    result = "Inserted unfulfilled in dynamodb"
+                except IOError:
+                    print("no response from dynamodb")
+            assert result == "Inserted fulfilled in dynamodb"
+            print result
+    except EOFError:
+        print("lambda handler failed for some error")
 
 
 def test_details_marketo():
-    email_test = 'daniel.taylor@alfresco.com'
-    result = details_marketo(email_test)
-    expected_result = {'result': [{'updatedAt': '2015-08-10T06:53:11Z', 'lastName': 'Taylor', 'firstName': 'Dan', 'createdAt': '2014-09-18T20:56:57Z', 'email': 'daniel.taylor@alfresco.com', 'id': 1558511}], 'success': True, 'requestId': 'e809#14f22884e5f'}
-    assert result == expected_result
-    print("test passed")
+    try:
+        email_test = 'daniel.taylor@alfresco.com'
+        result = details_marketo(email_test)
+        expected_result = {'result': [{'updatedAt': '2015-08-10T06:53:11Z', 'lastName': 'Taylor', 'firstName': 'Dan', 'createdAt': '2014-09-18T20:56:57Z', 'email': 'daniel.taylor@alfresco.com', 'id': 1558511}], 'success': True, 'requestId': 'e809#14f22884e5f'}
+        assert result == expected_result
+        print("test passed")
+    except IOError:
+        print("test failed. an error has been found")
 
 @mock_dynamodb2
 def test_insert_into_dynamo():
-    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+    dynamodb = boto3.client('dynamodb', region_name='us-west-2')
 
     table_t = dynamodb.create_table(
     TableName='trial_request_table',
     KeySchema=[
         {
             'AttributeName': 'LeadId',
-            'KeyType': 'HASH'  #Partition key
+            'KeyType': 'HASH'  # Partition key
         },
         {
             'AttributeName': 'Date',
-            'KeyType': 'RANGE'  #Sort key
+            'KeyType': 'RANGE'  # Sort key
         }
     ],
     AttributeDefinitions=[
@@ -121,32 +131,85 @@ def test_insert_into_dynamo():
         'WriteCapacityUnits': 10
     })
 
+    try:
+        curr_date = time.strftime("%d/%m/%Y")
+        lead_id = "daniel.taylor@alfresco.com"
+        response_m = {'result': [{'updatedAt': '2015-08-10T06:53:11Z', 'lastName': 'Taylor', 'firstName': 'Dan', 'createdAt': '2014-09-18T20:56:57Z', 'email': 'daniel.taylor@alfresco.com', 'id': 1558511}], 'success': True, 'requestId': 'e809#14f22884e5f'}
+        fulfilled_test = 'y'
+        count_attemps = 10
+        if response_m == "":
+            response = dynamodb.update_item(
+                TableName='trial_request_table',
+                Key={
+                    'LeadId': {"N": str(lead_id)},
+                    'Date': {"S": curr_date}
+                },
+                UpdateExpression= "set #Fulfilled = :f, #Attemps = :a, #RequestTime = :rt, #RequestMsg = :rm",
+                ExpressionAttributeNames={
+                    '#Fulfilled': "Fulfilled",
+                    '#Attemps': "Attemps",
+                    '#RequestTime': "RequestTime",
+                    '#RequestMsg': "RequestMsg"
+                },
+                ExpressionAttributeValues={
+                    ':f': {"S": fulfilled_test},
+                    ':a': {"N": str(count_attemps)},
+                    ':rt': {"N": '0'},
+                    ':rm': {"S": str(response_m)}
+            })
+            print("UpdateItem succeeded:")
+            return response
+        else:
+            dt = datetime.datetime.strptime(response_m['result'][0]['createdAt'], "%Y-%m-%dT%H:%M:%SZ")
+            request_time = time.mktime(dt.timetuple()) + (dt.microsecond / 1000000.0)
+            request_time = int(request_time)
+            response = dynamodb.update_item(
+                TableName='trial_request_table',
+                Key={
+                    'LeadId': {"N": str(lead_id)},
+                    'Date': {"S": curr_date}
+                },
+                UpdateExpression= "set #Fulfilled = :f, #Attemps = :a, #RequestTime = :rt, #RequestMsg = :rm",
+                ExpressionAttributeNames={
+                    '#Fulfilled': "Fulfilled",
+                    '#Attemps': "Attemps",
+                    '#RequestTime': "RequestTime",
+                    '#RequestMsg': "RequestMsg"
+                },
+                ExpressionAttributeValues={
+                    ':f': {"S": fulfilled_test},
+                    ':a': {"N": str(count_attemps)},
+                    ':rt': {"N": str(request_time)},
+                    ':rm': {"S": str(response_m)}
+            })
+            print("UpdateItem succeeded:")
+            return response
 
-
-    lead_id = "daniel.taylor@alfresco.com"
-    response_m = {'result': [{'updatedAt': '2015-08-10T06:53:11Z', 'lastName': 'Taylor', 'firstName': 'Dan', 'createdAt': '2014-09-18T20:56:57Z', 'email': 'daniel.taylor@alfresco.com', 'id': 1558511}], 'success': True, 'requestId': 'e809#14f22884e5f'}
-    fulfilled_test = 'y'
-    count_attemps = 10
-    result = insert_into_dynamo(table_t,lead_id,response_m,fulfilled_test,count_attemps)
-
-    assert result['ResponseMetadata']['HTTPStatusCode'] == 200
-    print("test passed")
+        assert result['ResponseMetadata']['HTTPStatusCode'] == 200
+        print("test passed")
+    except IOError:
+        print("test failed. an error has been found")
 
 
 @mock_sqs
 def test_send_to_SQS():
-    response_m = {'result': [{'updatedAt': '2015-08-10T06:53:11Z', 'lastName': 'Taylor', 'firstName': 'Dan', 'createdAt': '2014-09-18T20:56:57Z', 'email': 'daniel.taylor@alfresco.com', 'id': 1558511}], 'success': True, 'requestId': 'e809#14f22884e5f'}
-    sqs = boto3.resource('sqs', region_name='us-west-2')
-    queue = sqs.create_queue(QueueName='OnlineTrialRequestSQS', Attributes={'DelaySeconds': '5'})
-    resp = queue.send_message(MessageBody=json.dumps(response_m))
-    assert resp['ResponseMetadata']['HTTPStatusCode'] == 200
-    print(resp)
-    print(resp.get('MessageId'))
-    print(resp.get('MD5OfMessageBody'))
-    print("test passed")
+    try:
+        response_m = {'result': [{'updatedAt': '2015-08-10T06:53:11Z', 'lastName': 'Taylor', 'firstName': 'Dan', 'createdAt': '2014-09-18T20:56:57Z', 'email': 'daniel.taylor@alfresco.com', 'id': 1558511}], 'success': True, 'requestId': 'e809#14f22884e5f'}
+        sqs = boto3.client('sqs', region_name='us-west-2')
+        queue = sqs.create_queue(QueueName='OnlineTrialRequestSQS', Attributes={'DelaySeconds': '5'})
+        queue_url_message = sqs.get_queue_url(QueueName='OnlineTrialRequestSQS')
+        queue_url = queue_url_message['QueueUrl']
+        resp = sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(response_m))
+        assert resp['ResponseMetadata']['HTTPStatusCode'] == 200
+        print(resp)
+        print(resp.get('MessageId'))
+        print(resp.get('MD5OfMessageBody'))
+        print("test passed")
+    except IOError:
+        print("test failed. an error has been found")
 
 
-#test_details_marketo()
+# test_details_marketo()
 test_insert_into_dynamo()
 test_send_to_SQS()
 test_lambda_handler()
