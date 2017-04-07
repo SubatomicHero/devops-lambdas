27cacd87a1f10a31c72b59e583dd6b86d360ab56
Juena Ferdous <juena.ferdous@alfresco.com>
2017-04-07 13:32:14 +0100

    Lambda function for Trial Stack Builder: modification

--- python/TrialStackBuilder/TrialStackBuilderLambda.py
+++ python/TrialStackBuilder/TrialStackBuilderLambda.py
@@ -35,7 +35,7 @@ class TrialStackBuilder:
         try :
             for stack in stackList:
                 if stack['StackStatus']=="CREATE_IN_PROGRESS" :
-                    print("{} is still creating".format(stack['StackName']))
+                    print("The stack {} is still creating".format(stack['StackName']))
                     count +=1
                 elif stack['StackStatus']=="CREATE_COMPLETE" :
                     instanceId = self.findInstanceId(stack['Outputs'])
@@ -49,6 +49,7 @@ class TrialStackBuilder:
                         raise TypeError
                     if unassigned == True :   
                         count += 1 
+                        print("The stack {} exists which is a unassigned stack.".format(stack['StackName']))
         except Exception as err:
             message = "{0}\n".format(err)
             print(message)
@@ -108,7 +109,7 @@ class TrialStackBuilder:
                 
     def createStack(self):
         name = str(uuid.uuid1())
-        name =  name.replace('-','')
+        name =  'TrialStack'+name.replace('-','')
         branch = 'develop' if os.environ['stage'] == 'test' else 'master'
         filename = "online-trial-stack-{}.yaml".format(branch)
         URL = "https://s3.amazonaws.com/{}/{}".format(os.environ['template_bucket_name'], filename)
@@ -133,43 +134,49 @@ class TrialStackBuilder:
                 print(message)
                 return None
         
+    def run(self,event):
+        try :
+            if event['source']== None:
+                raise TypeError
+            try :
+                stack_count = ast.literal_eval(os.environ['stack_count'])
+            except Exception as err:
+                raise err
+                
+            source = event['source']
+            if source == 'aws.events':
+                stackList = TSB.listStack()
+                numberStack = TSB.countUnassignedStack(stackList)
+                if numberStack == None:
+                    raise TypeError
+                if(numberStack < stack_count):
+                    stackToCreate = stack_count - numberStack 
+                    print ('Number of stacks to be created: '+ str(stackToCreate))
+                    for i in range(stackToCreate):
+                        response = TSB.createStack()
+                        if response == None:
+                            raise TypeError
+                else :
+                    print ('Number of stacks to be created: '+ str(0))
+                    print("All OK")
+                    return 200
+            
+            else:
+                TSB.createStack()
+    
+        except Exception as err:
+            message = "{0}\n".format(err)
+            print(message)
+            return ('Failure due to : '+  "' ' ".join(str(x) for x in err.args))
+        else:
+            print("All OK")
+            return 200
+        
 TSB = TrialStackBuilder()
 
 def lambda_handler(event, context):
-    try :
-        if event['source']== None:
-            raise TypeError
-        try :
-            stack_count = ast.literal_eval(os.environ['stack_count'])
-        except Exception as err:
-            raise err
-            
-        source = event['source']
-        if source == 'aws.events':
-            stackList = TSB.listStack()
-            numberStack = TSB.countUnassignedStack(stackList)
-            if numberStack == None:
-                raise TypeError
-            if(numberStack < stack_count):
-                stackToCreate = stack_count - numberStack 
-                print ('Number of stacks to be created: '+ str(stackToCreate))
-                for i in range(stackToCreate):
-                    response = TSB.createStack()
-                    if response == None:
-                        raise TypeError
-            else :
-                print ('Number of stacks to be created: '+ str(0))
-                print("All OK")
-                return 200
-        
-        else:
-            TSB.createStack()
-
+    try:
+        return TSB.run(event)
     except Exception as err:
-        message = "{0}\n".format(err)
-        print(message)
-        return ('Failure due to : '+  "' ' ".join(str(x) for x in err.args))
-    else:
-        print("All OK")
-        return 200
-        
+        print("{}".format(err))
+    return 1
