31bfb142367cbfaab1851e49c48746d05d94bd5d
Daniel Monteiro <daniel.monteiro@alfresco.com>
2017-04-10 12:54:52 +0100

    updates on code review

--- python/TrialRequestHandler/TRH.py
+++ python/TrialRequestHandler/TRH.py
@@ -6,15 +6,15 @@ import os
 import boto3
 import time
 
+dynamo_client = boto3.client('dynamodb')
+sqs_client = boto3.client('sqs')
+sns_client = boto3.client('sns')
+
 print('Getting message from SNS')
 
 # main lambda function
 def lambda_handler(event, context):
     try:
-        dynamo_client = boto3.client('dynamodb')
-        sqs_client = boto3.client('sqs')
-        sns_client = boto3.client('sns')
-
         handler = TrialRequestHandler()
         message = event['Records'][0]['Sns']['Message']
         get_source = message['source']
@@ -25,7 +25,8 @@ def lambda_handler(event, context):
             count_attempts = 0
             while not response:
                 try:
-                    response = handler.details_marketo(lead_id,os.environ['api_host'], os.environ['client_id'], os.environ['client_secret'])
+                    response = handler.details_marketo(lead_id)
+                    print("details received from marketo: {}".format(response))
                     count_attempts = count_attempts + 1
                     if count_attempts == 15:
                         break
@@ -34,26 +35,26 @@ def lambda_handler(event, context):
             if response:
                 try:
                     fulfilled_test = 'y'
-                    ins_db = handler.insert_into_dynamo(dynamo_client, os.environ['trial_request_table'],lead_id, response, fulfilled_test, count_attempts)
+                    ins_db = handler.insert_into_dynamo(lead_id, response, fulfilled_test, count_attempts)
                     if ins_db:
-                        ins_sqs = handler.send_to_SQS(sqs_client, os.environ['sqs_url'], response)
+                        ins_sqs = handler.send_to_SQS(response)
                         if ins_sqs:
                             return "Inserted fulfilled in dynamodb and SQS"
                         return "failed to insert in SQS"
                     return "failed to insert into dynamodb"
                 except IOError:
-                    print("no response from dynamodb")
+                    print("no response from dynamodb (fulfilled)")
             else:
                 try:
                     fulfilled_test = 'n'
-                    ins_db = handler.insert_into_dynamo(dynamo_client, os.environ['trial_request_table'],lead_id, response, fulfilled_test, count_attempts)
+                    ins_db = handler.insert_into_dynamo(lead_id, response, fulfilled_test, count_attempts)
                     if ins_db:
                         # notify IM solution
                         return "Inserted unfulfilled in dynamodb"
                     return "failed to insert into dynamodb"
 
                 except IOError:
-                    print("no response from dynamodb")
+                    print("no response from dynamodb (unfulfilled)")
     except EOFError:
         print("lambda handler failed")
 
@@ -61,12 +62,9 @@ def lambda_handler(event, context):
 class TrialRequestHandler:
 
     # function to obtain the details regarding the lead_id passed as a parameter
-    def details_marketo(self,lead_id, ah, ci, cs):
+    def details_marketo(self,lead_id):
         try:
-            api_host = ah
-            client_id = ci
-            client_secret = cs
-            m = Marketo(host=api_host, client_id=client_id, client_secret=client_secret)
+            m = Marketo(host=os.environ['api_host'], client_id=os.environ['client_id'], client_secret=os.environ['client_secret'])
             response_m = m.get_leads('id', lead_id)
             if response_m:
                 return response_m
@@ -75,12 +73,12 @@ class TrialRequestHandler:
             print("an error has been found with the details")
 
     # function to insert the request in the dynamodB table for the Trial Request Handler
-    def insert_into_dynamo(self,dyn_db, table_id,lead_id, response_m, fulfilled_test, count_attempts):
+    def insert_into_dynamo(self, lead_id, response_m, fulfilled_test, count_attempts):
         try:
             curr_date = time.strftime("%d/%m/%Y")
             if not response_m:
-                response = dyn_db.update_item(
-                    TableName=table_id,
+                response = dynamo_client.update_item(
+                    TableName=os.environ['trial_request_table'],
                     Key={
                         'LeadId': {"N": str(lead_id)},
                         'Date': {"S": curr_date}
@@ -105,10 +103,9 @@ class TrialRequestHandler:
                 return None
             else:
                 dt = datetime.strptime(response_m['result'][0]['createdAt'], "%Y-%m-%dT%H:%M:%SZ")
-                request_time = time.mktime(dt.timetuple()) + (dt.microsecond / 1000000.0)
-                request_time = int(request_time)
-                response = dyn_db.update_item(
-                    TableName=table_id,
+                request_time = int(time.mktime(dt.timetuple()) + (dt.microsecond / 1000000.0))
+                response = dynamo_client.update_item(
+                    TableName=os.environ['trial_request_table'],
                     Key={
                         'LeadId': {"N": str(lead_id)},
                         'Date': {"S": curr_date}
@@ -131,13 +128,13 @@ class TrialRequestHandler:
                     return response
                 print("UpdateItem Failed:")
                 return None
-        except IOError:
-            print("an error has been found. Data not inserted into the table")
+        except:
+            raise IOError('an error has been found. Data not inserted into the table')
 
     # function to send the response from marketo to the SQS
-    def send_to_SQS(self,sqs_serv,sqs_queue,response):
+    def send_to_SQS(self,response):
         try:
-            resp = sqs_serv.send_message(QueueUrl=sqs_queue, MessageBody=json.dumps(response))
+            resp = sqs_client.send_message(QueueUrl=os.environ['sqs_url'], MessageBody=json.dumps(response))
             if resp:
                 return resp
             return None
