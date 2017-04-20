f10bb0189a7d947d1d8dd0d96dc7a5c38e7e0989
juena <jferdous@MBP512-JFERDOUS-0217.local>
2017-04-20 11:50:03 +0100

    added functions to FH + test

--- python/FulfillmentHandler/FulfillmentHandlerLambda.py
+++ python/FulfillmentHandler/FulfillmentHandlerLambda.py
@@ -9,31 +9,54 @@ from __future__ import print_function
 import json, os, boto3, json, six, ast
 
 class FulfillmentHandler:
-    def __init__(self):
+    def __init__(self, table = os.environ['trial_request_table']):
         try:
             self.sns_client = boto3.client('sns')
             self.dynamo_client = boto3.client('dynamodb')
+            self.trial_request_table = table
         except Exception as err:
-            message = "{0}\n".format(err)
-            print(message)
+            print("{}\n".format(err))
             raise err
+            
+    def readFromTable(self, table):
+        try:
+            response = self.dynamo_client.get_item(
+                TableName = table.name,
+                Key={
+                    'leadID': {
+                        'S' : 'n'
+                    }
+
+                }
+            )
+            return response
+        except Exception as err:
+            print("{}\n".format(err))
+        else:
+            return None
 
     def run(self):
         try :
-            print (0)
-        except Exception as err:
-            message = "{0}\n".format(err)
-            print(message)
-            return ('Failure due to : '+  "' ' ".join(str(x) for x in err.args))
-        else:
-            print("All OK")
+            resp = self.readFromTable(self.trial_request_table)
+            if resp is None:
+                raise ValueError('Cannot read fron table')
+            print ('All ok')
             return 200
+        except Exception as err:
+            print("{}\n".format(err))
+        else:
+            return None
         
 FH = FulfillmentHandler()
 
 def lambda_handler(event, context):
     try:
-        return FH.run()
+        res = FH.run()
+        if res is None:
+            raise ValueError('The function has failed')
+        print("All OK")
+        return 200
     except Exception as err:
         print("{}".format(err))
-        return 1
+    else:
+        return None
