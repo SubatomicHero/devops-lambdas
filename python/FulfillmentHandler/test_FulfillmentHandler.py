5dbcd1bb72612acc8494fcd229966ba066f143db
juena <jferdous@MBP512-JFERDOUS-0217.local>
2017-04-20 18:17:32 +0100

    modifications

--- python/FulfillmentHandler/test_FulfillmentHandler.py
+++ python/FulfillmentHandler/test_FulfillmentHandler.py
@@ -1,7 +1,7 @@
 import boto, boto3, botocore.exceptions, requests, sure , time, json, unittest
 from boto.exception import SQSError
 from boto.sqs.message import RawMessage, Message
-from moto import  mock_sqs, mock_ec2, mock_cloudformation, mock_dynamodb
+from moto import  mock_sqs, mock_ec2, mock_cloudformation, mock_dynamodb2
 from nose.tools import assert_raises
 from FulfillmentHandlerLambda import FulfillmentHandler
 
@@ -11,41 +11,62 @@ class TestFulfillmentHandlerLambda(unittest.TestCase):
     def test_instance(self):
         self.assertIsNotNone(FH.sns_client)
         self.assertIsNotNone(FH.dynamo_client)
+
     
-    @mock_dynamodb
-    def test_rereadFromTable(self):
-        dynamodb = boto.connect_dynamodb()
-        # response = dynamo_client.create_table(
-        #     TableName='table',
-        #      AttributeDefinitions=[
-        #       {
-        #           'AttributeName': 'LeadId',
-        #           'AttributeType': 'S',
-        #       },
-        #       ],
-        #       KeySchema=[
-        #           {
-        #               'AttributeName': 'LeadId',
-        #               'KeyType': 'HASH',
-        #           }
-        #       ],
-        #       ProvisionedThroughput={
-        #           'ReadCapacityUnits': 5,
-        #           'WriteCapacityUnits': 5,
-        #       },
-        #   )
-        table_schema = dynamodb.create_schema(
-            hash_key_name='leadID',
-            hash_key_proto_value=str,
-            range_key_name='subject',
-            range_key_proto_value=str
-        )
-        table = dynamodb.create_table(
-            name='test_table',
-            schema=table_schema,
-            read_units=10,
-            write_units=10
-        )
-        FH.readFromTable(table)
+    @mock_dynamodb2
+    def createTable(self):
+        try:
+            dynamodb = boto3.client('dynamodb', region_name='us-east-1')
+            table = dynamodb.create_table(
+                TableName = 'trial_request_table',
+                KeySchema = [
+                    {
+                        'AttributeName': 'LeadId',
+                        'KeyType': 'HASH'  # Partition key
+                    },
+                    {
+                        'AttributeName': 'Date',
+                        'KeyType': 'RANGE'  # Sort key
+                    }
+                ],
+                AttributeDefinitions = [
+                    {
+                        'AttributeName': 'LeadId',
+                        'AttributeType': 'N'
+                    },
+                    {
+                        'AttributeName': 'Date',
+                        'AttributeType': 'S'
+                    },
+                    {
+                        'AttributeName': 'Fulfilled',
+                        'AttributeType': 'S'
+                    },
+                    {
+                        'AttributeName': 'Attempts',
+                        'AttributeType': 'N'
+                    },
+                    {
+                        'AttributeName': 'RequestTime',
+                        'AttributeType': 'N'
+                    },
+                    {
+                        'AttributeName': 'Request',
+                        'AttributeType': 'S'
+                    },
+                ],
+                ProvisionedThroughput = {
+                    'ReadCapacityUnits': 10,
+                    'WriteCapacityUnits': 10
+                }
+            )
+            return dynamodb
+        except IOError:
+            print("test failed. no instance of dynamodb created")
+        
+    def test_readFromTable(self):
+        dynamodb = self.createTable()
+        print(FH.readFromTable(dynamodb, 'trial_request_table'))
+       
 if __name__ == '__main__':
   unittest.main()
