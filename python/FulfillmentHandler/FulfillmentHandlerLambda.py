182c9b916582bf3d6792c1602c6131136168fb04
Juena Ferdous <juena.ferdous@alfresco.com>
2017-04-24 17:16:26 +0100

    test for the lambda

--- python/FulfillmentHandler/FulfillmentHandlerLambda.py
+++ python/FulfillmentHandler/FulfillmentHandlerLambda.py
@@ -6,30 +6,63 @@
 # // (Fulfillment is satisfied by the trial request handler, not this handler)
 
 from __future__ import print_function
-import json, os, boto3, json, six, ast
+import json, os, boto3, json, six, ast, time
 
 os.environ['trial_request_table'] = '!Ref OnlineTrialRequestDynamoDbTable'
+os.environ['topic_arn'] = '!Ref OnlineTrialRequestSNSTopic'
 
 class FulfillmentHandler:
-    def __init__(self, table = os.environ['trial_request_table']):
+    def __init__(self):
         try:
-            self.sns_client = boto3.client('sns')
-            self.dynamo_client = boto3.client('dynamodb')
-            self.trial_request_table = table
+            self.sns_res = boto3.resource('sns')
+            self.dynamo_res = boto3.resource('dynamodb')
         except Exception as err:
             print("{}\n".format(err))
             raise err
             
     def readFromTable(self, dynamodb, name):
         try:
-            response = dynamodb.get_item(
-                TableName = name,
-                Key={
+            table = dynamodb.Table(name)
+            response = table.scan(
+                ScanFilter = {
                     'Fulfilled': {
-                        'S' : 'n'
+                        'AttributeValueList': [
+                            'n'
+                        ],
+                        'ComparisonOperator': 'EQ'
                     }
+                },
+            )
+            if response['Items'] is None:
+                raise ValueError('No item found in the dynamoDb table')
+            elif len(response['Items']) == 0:
+                raise ValueError('No unfulfilled request found')
+            return response['Items'][0]['LeadId']
+        except Exception as err:
+            print("{}\n".format(err))
+        else:
+            return None
+    
+    def createMessageObject(self, leadId):
+        obj = { 
+            'source': {
+                'DataType': 'string',
+                'StringValue': 'onlinetrial'
+            },
+            'lead': {
+                'DataType': 'string',
+                'StringValue': str(leadId)
+            }
+        }
+        return obj
 
-                }
+    def publishTopicSNS(self, sns_client, topic, obj):
+        try:
+            topic = sns.Topic('arn:aws:sns:us-east-1:179276412545:online-trial-control-test-OnlineTrialRequestSNSTopic-3MIYCD0W0S9Z')
+            response = sns_client.publish(
+                TopicArn = 'arn:aws:sns:us-east-1:179276412545:online-trial-control-test-OnlineTrialRequestSNSTopic-3MIYCD0W0S9Z',
+                Message = 'string',
+                MessageAttributes = obj
             )
             return response
         except Exception as err:
@@ -38,11 +71,11 @@ class FulfillmentHandler:
             return None
 
     def run(self):
-        try :
-            resp = self.readFromTable(self.trial_request_table)
-            if resp is None:
+        try:
+            leadId = self.readFromTable(self.dynamo_client, os.environ['trial_request_table'])
+            if leadId is None:
                 raise ValueError('Cannot read fron table')
-            print ('All ok')
+            obj = self.createMessageObject(leadId)
             return 200
         except Exception as err:
             print("{}\n".format(err))
