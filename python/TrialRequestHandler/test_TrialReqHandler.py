df1bd5042a0357810e462b00fd1f748ba0c15b79
Juena Ferdous <juena.ferdous@alfresco.com>
2017-05-05 16:51:53 +0100

    refactoring and tests for insert_into_dynamo and send_to_sqs

--- python/TrialRequestHandler/test_TrialReqHandler.py
+++ python/TrialRequestHandler/test_TrialReqHandler.py
@@ -1,13 +1,13 @@
 import boto3
-from moto import mock_sqs
-from moto import mock_dynamodb2
+from moto import mock_sqs, mock_dynamodb2
 from TrialRequestHandlerLambda import TrialRequestHandler
 import unittest
 import datetime
 import time
 import json
 import os
-
+import requests
+import httpretty
 
 os.environ['sqs_url'] = ""
 os.environ['trial_request_table'] = ""
@@ -15,20 +15,35 @@ os.environ['api_host'] = ""
 os.environ['client_id'] = ""
 os.environ['client_secret'] = ""
 
+response = {'result': [{'updatedAt': '2015-08-10T06:53:11Z', 'lastName': 'Taylor', 'firstName': 'Dan', 'createdAt': '2014-09-18T20:56:57Z', 'email': 'daniel.taylor@alfresco.com', 'id': 1558511}], 'success': True, 'requestId': 'e809#14f22884e5f'}
+TRH = TrialRequestHandler()
+
 class TestTrialRequestHandler(unittest.TestCase):
-    def test_details_marketo():
-        try:
-            id_test = '89406'
-            handler = TrialRequestHandler()
-            os.environ['api_host'] = "https://453-liz-762.mktorest.com/"
-            os.environ['client_id'] = "35a7e1a3-5e60-40b2-bd54-674680af2adc"
-            os.environ['client_secret'] = "iPPgKiB224jsa02duwPcKy9ox7078P7S"
-            result = handler.details_marketo(id_test)
-            expected_result = {"requestId": "aa09#15b3ee9bc02", "success": True, "result": [{"firstName": "UNKNOWN", "lastName": "UNKNOWN", "id": 89406, "updatedAt": "2017-03-16T11:48:42Z", "email": "daniel.taylor@alfresco.com", "createdAt": "2016-08-23T11:30:55Z"}]}
-            assert result == expected_result
-            print("test passed")
-        except IOError:
-            print("test failed. an error has been found")
+    def test_instance(self):
+        self.assertIsNotNone(TRH.dynamo_client)
+        self.assertIsNotNone(TRH.sqs_client)
+
+    # @httpretty.activate
+    # def test_assign_user_to_stack(self):
+    #     httpretty.register_uri(
+    #         httpretty.GET,
+    #         "https://453-liz-762.mktorest.com/identity/oauth/token",
+    #         body=json.dumps(response)
+    #     )
+    #     r =  TRH._get_access_token('https://453-liz-762.mktorest.com/')
+        # self.assertTrue(response)
+
+    # def test_details_marketo(self):
+    #     try:
+    #         id_test = '89406'
+    #         print ('f')
+    #         result = TRH.details_marketo(id_test)
+    #         expected_result = {"requestId": "aa09#15b3ee9bc02", "success": True, "result": [{"firstName": "UNKNOWN", "lastName": "UNKNOWN", "id": 89406, "updatedAt": "2017-03-16T11:48:42Z", "email": "daniel.taylor@alfresco.com", "createdAt": "2016-08-23T11:30:55Z"}]}
+    #         print (result)
+    #         assert result == expected_result
+    #         print("test passed")
+    #     except IOError:
+    #         print("test failed. an error has been found")
 
     @mock_dynamodb2
     def test_dynamo_service(self):
@@ -84,40 +99,37 @@ class TestTrialRequestHandler(unittest.TestCase):
 
     @mock_dynamodb2
     def test_insert_dynamo(self):
-        response = {'result': [{'updatedAt': '2015-08-10T06:53:11Z', 'lastName': 'Taylor', 'firstName': 'Dan', 'createdAt': '2014-09-18T20:56:57Z', 'email': 'daniel.taylor@alfresco.com', 'id': 1558511}], 'success': True, 'requestId': 'e809#14f22884e5f'}
         leadid = 89406
         fulfilled_test = "y"
         count_attempts = 10
-        handler = TrialRequestHandler()
         self.test_dynamo_service()
-        resp = handler.insert_into_dynamo(leadid, response, fulfilled_test, count_attempts)
-        assert resp['ResponseMetadata']['HTTPStatusCode'] == 200
-        print("test passed")
-
-    @mock_sqs
-    def test_SQS_service(self):
-        try:
-            sqs = boto3.client('sqs')
-            queue = sqs.create_queue(QueueName='OnlineTrialRequestSQS', Attributes={'DelaySeconds': '5'})
-            queue_url_message = sqs.get_queue_url(QueueName='OnlineTrialRequestSQS')
-            queue_url = queue_url_message['QueueUrl']
-            os.environ['sqs_url'] = queue_url
-            return sqs
-        except IOError:
-            print("test failed. no instance of sqs created")
-
+        resp = TRH.insert_into_dynamo(leadid, response, fulfilled_test, count_attempts)
+        assert resp == True
+        print("Test insert_into_dynamo: passed")
+    
     @mock_sqs
     def test_send_to_SQS(self):
         try:
-            response_m = {'result': [{'updatedAt': '2015-08-10T06:53:11Z', 'lastName': 'Taylor', 'firstName': 'Dan', 'createdAt': '2014-09-18T20:56:57Z', 'email': 'daniel.taylor@alfresco.com', 'id': 1558511}], 'success': True, 'requestId': 'e809#14f22884e5f'}
-            sqs_s = test_SQS_service()
-            handler = TrialRequestHandler()
-            resp = handler.send_to_SQS(sqs_s, response_m)
+            sqs = boto3.resource('sqs', region_name = 'us-east-1')
+            q1 = sqs.create_queue(QueueName = 'publishqueue')
+            publish_queue = sqs.get_queue_by_name(QueueName = 'publishqueue')
+            resp = TRH.send_to_SQS(publish_queue.url, response)
             assert resp['ResponseMetadata']['HTTPStatusCode'] == 200
-            print(resp)
-            print(resp.get('MessageId'))
-            print(resp.get('MD5OfMessageBody'))
-            print("test passed")
+            assert resp['ResponseMetadata']["RetryAttempts"] == 0
+            assert resp.get('Failed') == None
+            print("Test 'send message' : passed")
+        except IOError:
+            print("test failed. an error has been found")
+
+    @mock_sqs
+    def test_send_to_SQS_returns_fail(self):
+        try:
+            sqs = boto3.resource('sqs', region_name = 'us-east-1')
+            q1 = sqs.create_queue(QueueName = 'publishqueue')
+            publish_queue = sqs.get_queue_by_name(QueueName = 'publishqueue')
+            response = TRH.send_to_SQS(publish_queue.url, None)
+            assert response == None
+            print("Test 'failed send message' : passed")
         except IOError:
             print("test failed. an error has been found")
 
